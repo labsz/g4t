e364429b05880fa5f90f71995c3853ec2707626b
freazesss <freazesss@gmail.com>
2020-08-26 09:13:24 -0300

    adding the 'show last commit' functionality

--- main.rb
+++ main.rb
@@ -113,6 +113,12 @@ class Options
       puts("Command: #{cmd}")
       system(cmd)
     end
+
+    def show_last_commit
+      cmd = "git show"
+      puts("Command: #{cmd}")
+      system(cmd)
+    end
 end
 
 class Application
@@ -161,7 +167,7 @@ class Application
 
   def show_panel
     options = ['Add remote address', 'Add files', 'Commit files', 'Push files to branch', 'Show git status', 'Show git logs']
-    options.push('Remove a file', 'Show diff', 'Change branch', 'Restore a file', 'Close')
+    options.push("Show the last commit", 'Remove a file', 'Show diff', 'Change branch', 'Restore a file', 'Close')
     begin
       @option = @prompt.select("#{$lastmsg}, what do you want to do?", options)
       panel_verify
@@ -192,6 +198,8 @@ class Application
       @opt.change_branch
     when 'Remove a file' then
       @opt.remove_file
+    when 'Show the last commit' then
+      @opt.show_last_commit
     else
       abort("Goodbye, closed.")
     end
